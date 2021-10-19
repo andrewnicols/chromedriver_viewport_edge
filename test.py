d5a29626fce9f2435fc134caa6a2bf80f94c2ff3
Andrew Nicols <andrew@nicols.co.uk>
2021-10-19 16:08:05 +0800

    Change URLs

--- test.py
+++ test.py
@@ -10,11 +10,11 @@ options.add_argument("--window-size=1366,707")
 options.add_argument("--disable-dev-shm-usage")
 options.headless = False
 driver = webdriver.Remote(
-    command_executor="http://127.0.0.1:4444/wd/hub",
+    command_executor="http://127.0.0.1:4444",
     options=options,
 )
 
-driver.get("http://host.docker.internal:8880/index.html")
+driver.get("http://localhost:8880/index.html")
 elem = driver.find_element(By.LINK_TEXT, "Click me")
 driver.save_screenshot('before.png')
 elem.click()
